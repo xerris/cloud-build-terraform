b46875d52364f166643d611bffcd381090a3a94c
Jonathan McCracken <jonathan@xerris.com>
2020-06-15 13:43:31 -0600

    adding backend

--- environments/sandbox/main.tf
+++ environments/sandbox/main.tf
@@ -1,3 +1,11 @@
+        
+terraform{
+    backend "gcs" {
+        bucket         = "tf-state-sandbox-1"
+        prefix         = "enrichme-project/sandbox"
+    }
+}
+
 module "common_shared_module" {
   source            = "../../modules"
 }
