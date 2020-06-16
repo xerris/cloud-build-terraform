d9f4786cdc1cfe4148c2439e6be13f0cf9ff945c
Jonathan McCracken <jonathan@xerris.com>
2020-06-16 16:49:27 -0600

    test

---
+++ environments/staging/main.tf
@@ -0,0 +1,18 @@
+        
+terraform{
+  required_version = "0.12.26"
+
+    backend "gcs" {
+        bucket         = "tf-state-sandbox-1"
+        prefix         = "enrichme-project/sandbox"
+    }
+}
+
+module "common_shared_module" {
+  source            = "../../modules"
+}
+
+module "vm"{
+  source  = "../../modules/vm"
+  vm_name = var.vm_name
+}
