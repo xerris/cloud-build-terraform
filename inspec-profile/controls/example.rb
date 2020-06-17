4cd7805a8375b22a5106275eff9f1f2c7b76b6e5
John Mitchell <john@johntellsall.com>
2020-06-17 16:30:20 -0700

    reset: re-init via https://github.com/inspec/inspec-gcp/

--- inspec-profile/controls/example.rb
+++ inspec-profile/controls/example.rb
@@ -1,18 +1,27 @@
 # copyright: 2018, The Authors
 
-title "sample section"
+title "Sample Section"
 
-# you can also use plain tests
-describe file("/tmp") do
-  it { should be_directory }
-end
+gcp_project_id = attribute("gcp_project_id")
 
 # you add controls here
-control "tmp-1.0" do                        # A unique ID for this control
-  impact 0.7                                # The criticality, if this control fails.
-  title "Create /tmp directory"             # A human-readable title
+control "gcp-single-region-1.0" do                                                    # A unique ID for this control
+  impact 1.0                                                                          # The criticality, if this control fails.
+  title "Ensure single region has the correct properties."                            # A human-readable title
   desc "An optional description..."
-  describe file("/tmp") do                  # The actual test
-    it { should be_directory }
+  describe google_compute_region(project: gcp_project_id, name: "europe-west2") do    # The actual test
+    its("zone_names") { should include "europe-west2-a" }
+  end
+end
+
+# plural resources can be leveraged to loop across many resources
+control "gcp-regions-loop-1.0" do                                                     # A unique ID for this control
+  impact 1.0                                                                          # The criticality, if this control fails.
+  title "Ensure regions have the correct properties in bulk."                         # A human-readable title
+  desc "An optional description..."
+  google_compute_regions(project: gcp_project_id).region_names.each do |region_name|  # Loop across all regions by name
+    describe google_compute_region(project: gcp_project_id, name: region_name) do     # The test for a single region
+      it { should be_up }
+    end
   end
 end
