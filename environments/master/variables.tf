b382c4e61b2fdf5dee5166991e9cc2306c04e4f2
Jonathan McCracken <jonathan@xerris.com>
2020-06-17 08:06:42 -0600

    variable defaults removed

--- environments/master/variables.tf
+++ environments/master/variables.tf
@@ -1,12 +1,9 @@
 variable "vm_name" {
   type    = string
-  default = ""
 }
 variable "project_name"{
   type    = string
-  default = ""
 }
 variable "branch_name"{
   type    = string
-  default = ""
 }
--- environments/staging/variables.tf
+++ environments/staging/variables.tf
@@ -1,14 +1,11 @@
 variable "vm_name" {
   type    = string
-  default = ""
 }
 
 variable "project_name"{
   type    = string
-  default = ""
 }
 
 variable "branch_name"{
   type    = string
-  default = ""
 }
