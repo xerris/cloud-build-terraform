e01ad9087fba38467564e9ca71ffc9049baf130d
John Mitchell <john@johntellsall.com>
2020-06-22 14:41:59 -0700

    trace script

--- build/run-tflint.sh
+++ build/run-tflint.sh
@@ -4,6 +4,8 @@
 #
 set -euo pipefail # strict mode
 
+set -o verbose
+
 tf_directories=$(find . -type f -name '*.tf' | xargs -n1 dirname | uniq)
 echo $tf_directories
 echo $tf_directories | xargs -n1 tflint
